71b6e53f1af84192ad6dc8ed06f99220a7e3e15c
MKuliskova <michaela.kuliskova@email.cz>
2021-02-18 21:00:31 +0100

    Ukol 1

---
+++ 1/priklad02.py
@@ -0,0 +1,18 @@
+sklad = {
+    "1N4148": 250,
+    "BAV21": 54,
+    "KC147": 147,
+    "2N7002": 97,
+    "BC547C": 10
+}
+kod = str(input("Zadej kod soucastky: "))
+mnozstvi = int(input("Zadej mnozstvi: "))
+if kod in sklad:
+    if mnozstvi > sklad[kod]:
+        print(f"Soucastka je skladem, lze prodat maximalne {sklad[kod]} ks.")
+        sklad.pop(kod)
+    elif mnozstvi <= sklad[kod]:
+        print("Soucastka je skladem v danem mnozstvi.")
+        sklad[kod] -= mnozstvi
+else:
+        print("Soucastka neni skladem.")

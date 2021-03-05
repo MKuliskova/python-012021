9c2bc4e8f33cdea6b64dfcec57906987a4d6bab5
MKuliskova <michaela.kuliskova@email.cz>
2021-03-05 15:19:56 +0100

    Treti domaci ukol

---
+++ 3/priklad11.py
@@ -0,0 +1,9 @@
+class Auto:
+    def __init__(self, number, type, kilometres, availibility):
+        self.number = number
+        self.type = type
+        self.kilometres = kilometres
+        self.availibility = True
+
+Peugeot = Auto("4A2 3020","Peugeot 403 Cabrio",47534,True)
+Skoda = Auto("1P3 4747","Škoda Octavia",41253,False)

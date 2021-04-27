009272acfb86c5ba96ec3980069da3c8fcf0d7a1
MKuliskova <michaela.kuliskova@email.cz>
2021-04-27 07:49:35 +0200

    Ukol 9

--- pujcovna/katalog/models.py
+++ pujcovna/katalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Auto(models.Model):
+  registracni_znacka = models.CharField(max_length=20)
+  znacka = models.CharField(max_length=100)
+  kilometry = models.IntegerField()
+  datum_kontrola = models.DateField()
+
+class Zakaznik(models.Model):
+  jmeno = models.CharField(max_length=100)
+  prijmeni = models.CharField(max_length=100)
+  cislo_ridicak = models.CharField(max_length=100)
+  datum_narozeni = models.DateField()
+
+class Vypujcka(models.Model):
+  zacatek = models.DateTimeField()
+  konec = models.DateTimeField()
+  cena = models.IntegerField()
+
+
